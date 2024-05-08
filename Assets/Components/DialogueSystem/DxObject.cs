53c706b00d6d0e8fbba908d1dcbb1f7c8444a5dc
Sell MJ, Jack <m.j.sell@st.hanze.nl>
2024-05-08 11:12:39 +0200

    added comments

--- Assets/Components/DialogueSystem/DxObject.cs
+++ Assets/Components/DialogueSystem/DxObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace DialogueSystem
+namespace DialogueSystem // create a list that can be accessed from other scripts: DialogueSystem.DialogueCondition.
 {
     public enum DialogueCondition
     {
@@ -13,15 +13,15 @@ namespace DialogueSystem
         Idle
     }
 }
+// create the ability to make this in the Create menu in your right click asset folders
 [CreateAssetMenu(fileName = "DialogueObject", menuName = "Dialogue System / NPC Dialogue Object", order = 0)]
 
-public class DxObject : ScriptableObject
+public class DxObject : ScriptableObject // scriptable object means you use this as a template for other scripts
 {
-    /*public DialogueSystem.DialogueCondition dialogueCondition;*/
     public List<string> dialogueText = new List<string>();
     
-    [Header("Condition at Resolution")]
-    public DialogueSystem.DialogueCondition endCondition;
+    [Header("Condition at Resolution")] // header to display in the Inspector (does not impact function).
+    public DialogueSystem.DialogueCondition endCondition; // I use this to determine where the code goes next.
 }
 
 
